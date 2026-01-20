eb46983a58e19541cd5cef72079ceb8cd113e6e0
Michael B <94670314+Mbialowas10@users.noreply.github.com>
2026-01-20 12:31:54 -0600

    new files added

---
+++ goodhello.py
@@ -0,0 +1,16 @@
+def hello():
+    """
+    Prints a simplet greeting. 
+    """
+    print("Hello, DevOps learner!")
+
+def bye():
+    """
+    Prints a goodbye message. 
+    """
+    print("Bye, DevOps learner!")
+
+
+if __name__ == "__main__":
+    hello()
+    bye()
